89a4979b7fe5aef0b8bcbf64b93dd54c58c7927d
TharshiniAshok <tharshiniashok3344@gmail.com>
2018-08-04 18:19:06 +0530

    Update Code

--- src/main/java/week2day1/CreLead.java
+++ src/main/java/week2day1/CreLead.java
@@ -9,7 +9,7 @@ public class CreLead extends Annotations {
 	public static void main(String[] args) {
 		
 		startApp("chrome","http://leaftaps.com/opentaps");
-		type(locateElement("id","username"),"DemoSalesManager");
+		type(locateElement("id","username"),"DemoSales");
 		type(locateElement("id","password"),"crmsfr");
 		
 		
